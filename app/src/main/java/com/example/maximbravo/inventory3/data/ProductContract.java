7c8084fbf6f569257b85e3dfc9fc545049247569
Maxim Bravo <maxim@alexbravo.com>
2016-12-22 14:53:07 -0800

    Add ProductContract.java to project

---
+++ app/src/main/java/com/example/maximbravo/inventory3/data/ProductContract.java
@@ -0,0 +1,18 @@
+package com.example.maximbravo.inventory3.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Maxim Bravo on 12/22/2016.
+ */
+
+public class ProductContract {
+    public static class ProductEntry implements BaseColumns {
+        public static final String TABLE_NAME = "store";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PRODUCT_NAME = "name";
+        public static final String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public static final String COLUMN_PRODUCT_PRICE = "price";
+    }
+}
