49bf493bd81ca7b82bd30a5decc8e46fe5d2e267
Maxim Bravo <maxim@alexbravo.com>
2016-12-22 14:47:37 -0800

    Add Toast showing that add product menu item was clicked

--- app/src/main/java/com/example/maximbravo/inventory3/MainActivity.java
+++ app/src/main/java/com/example/maximbravo/inventory3/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_add_product) {
+            Toast.makeText(this, "You clicked the Add Product menu item.", Toast.LENGTH_LONG).show();
             return true;
         }
 
